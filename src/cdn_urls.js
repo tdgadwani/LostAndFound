d8f0a1befe0ef5e06e0114d722d982c4082dd81b
Ravi <103064016+RaviSaini045@users.noreply.github.com>
2024-07-13 13:57:14 +0530

    complete cdn urls

--- src/cdn_urls.js
+++ src/cdn_urls.js
@@ -1,20 +1,34 @@
 
 
-// const CDN_URLS = {
-//     arrow : "https://console.cloudinary.com/pm/c-5419a50136dfee906249458d06afc0/media-explorer?assetId=02cce1d903c17c8a527d3b190c849ef4",
-//     createAccount : "",
-//     discount : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720549366/oasy1y1atacneziqj6dk.png",
-//     EditLogoPencil : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720549628/z2u3d345wqyiikzbke6v.png",
-//     EditProfileHeading : "",
-//     facebookLogo : "https://console.cloudinary.com/pm/c-5419a50136dfee906249458d06afc0/media-explorer?assetId=0c2ed9afabc75bd0b2ea2bbea13fc272",
-//     forgotPassword : "",
-//     imageIcon : "",
-//     instagram : "",
-//     location : "",
-//     LoginHeading : "",
-//     logo : "",
-//     passworResetHeading : "",
-    
-
-// };
-// export default CDN_URLS ;
+const CDN_URLS = {
+    add_dp_icon : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858199/dxv3kezghklfjllxrfvk.svg",
+    caption : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858271/d4m2qnd1vynmnkhrb41q.svg",
+    cross : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720859119/mo7y42bvprtkjywwtypz.svg",
+    Group_129 : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858340/p2mdqdp0j14s2dcejwq4.svg",
+    Home_follow : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858352/w4t23rxcb9vmhocrx31c.svg",
+    Home_LeaderBoard : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858366/mhghuya5lqhactoq80hv.svg",
+    Home_share : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858367/vdvdivbciglooq2wsaup.svg",
+    login_bg  : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858395/zmmdspjutkbk29qarf6s.svg",
+    LostHomepage : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858405/pc8aq513mrbqkp4d7acp.svg",
+    lostSample_1 : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858513/o7ca2lcaqlfw24rgikgd.svg",
+    Map_Icon : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858541/k4tysjagxvae2bvxifny.svg",
+    pencil1 : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858559/ttiaocn4i391ay6rnb0r.png",
+    ResetP : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858672/skeheheyeoxel3knnebj.svg",
+    RightIcon : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858734/uvlqpx2yzy0tnucqv6u2.svg",
+    sideArrow : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858741/gvripxfmjpgnyhofuopc.svg",
+    SignIn_Page : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720858756/algcagjim8uyhhbvmjd3.svg",
+    arrow : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720548384/utexg0piefmjyu8pkdbk.svg",
+    createAccount : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720549087/ftsw09vinbnpfyjhofmo.svg",
+    discount : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720549366/oasy1y1atacneziqj6dk.png",
+    EditLogoPencil : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720549628/z2u3d345wqyiikzbke6v.png",
+    EditProfileHeading : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720549703/krpzzwzjhorhav6exbd6.svg",
+    facebookLogo : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720549817/rqv0mrolhruqmgxxseln.svg",
+    forgotPassword : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720549901/klqfusqxi5vb8zogkyk3.svg",
+    imageIcon : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720550035/hzcnqpcncmmh90tej0r6.svg",
+    instagram : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720550152/ljytulyovwvq8gdmunfs.svg",
+    location : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720550242/tj3lzbnhwvrc5zbyqbks.svg",
+    LoginHeading : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720550323/jiyn0m8wtaqzlyb61ekv.svg",
+    logo : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720550354/dla8f3ol18a5agsekawy.svg",
+    passworResetHeading : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720550452/hi7taigashbpg4opugwu.svg"
+};
+export default CDN_URLS ;
