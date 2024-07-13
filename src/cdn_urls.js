7a80f0b8876181769afdf398046519275b1be13b
Ravi <103064016+RaviSaini045@users.noreply.github.com>
2024-07-13 13:12:27 +0530

    assets stored at cloudinary

---
+++ src/cdn_urls.js
@@ -0,0 +1,20 @@
+
+
+// const CDN_URLS = () =>{
+//     arrow : "https://console.cloudinary.com/pm/c-5419a50136dfee906249458d06afc0/media-explorer?assetId=02cce1d903c17c8a527d3b190c849ef4",
+//     createAccount : "",
+//     discount : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720549366/oasy1y1atacneziqj6dk.png",
+//     EditLogoPencil : "https://res.cloudinary.com/dlgrwtkck/image/upload/v1720549628/z2u3d345wqyiikzbke6v.png",
+//     EditProfileHeading : "",
+//     facebookLogo : "https://console.cloudinary.com/pm/c-5419a50136dfee906249458d06afc0/media-explorer?assetId=0c2ed9afabc75bd0b2ea2bbea13fc272",
+//     forgotPassword : "",
+//     imageIcon : "",
+//     instagram : "",
+//     location : "",
+//     LoginHeading : "",
+//     logo : "",
+//     passworResetHeading : "",
+    
+
+// };
+// export default CDN_URLS ;
