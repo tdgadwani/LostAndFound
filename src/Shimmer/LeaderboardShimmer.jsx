08b59281460909ab93e9020bbc3096f15f9959aa
AshuraZor0 <harshbodwal25@gmail.com>
2024-08-17 23:39:11 +0530

    leader board shimmer ui added

---
+++ src/Shimmer/LeaderboardShimmer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LeaderboardShimmer=()=> {
+  return (
+    <div className='h-screen w-screen'>LeaderboardShimmer</div>
+  )
+}
+
+export default LeaderboardShimmer
