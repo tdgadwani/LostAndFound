d78374243b4536055440180f8bfc9f403100bf19
Ravi <103064016+RaviSaini045@users.noreply.github.com>
2024-08-23 00:39:24 +0530

    main templates update

--- server/mailTemplates/otpTemplate.js
+++ server/mailTemplates/otpTemplate.js
@@ -1,4 +1,4 @@
-// import Logo from "../../src/assets/LogoMain.svg"
+
 
 const OtpTemp = (name,otp) => {
     return `<!DOCTYPE html lang="en">
@@ -76,9 +76,9 @@ const OtpTemp = (name,otp) => {
                 <img src="https://res.cloudinary.com/dlgrwtkck/image/upload/v1720550354/dla8f3ol18a5agsekawy.svg" alt="Foundify Logo">
             </div>
             <div class="content">
-                <p class="heading">Hi {name},</p>
+                <p class="heading">Hi ${name},</p>
                 <p>To complete your sign-in, please use the following One-Time Password (OTP):</p>
-                <p class="otp">{otp}</p>
+                <p class="otp">${otp}</p>
                 <p class="bottom">This OTP is valid for the next <span style="color: red; font-weight: bold; font-size: 16px;">5</span> minutes. Do not share this code with anyone.
                 If you did not request this code, please ignore this email or contact our support team immediately.</p>
             </div>
