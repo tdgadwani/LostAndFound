eb6006992f5247a32ba0fa23f338a8a1587c105f
RaviSaini045 <103064016+RaviSaini045@users.noreply.github.com>
2024-08-26 23:58:54 +0530

    Update LostItems.model.js

--- server/models/LostItems.model.js
+++ server/models/LostItems.model.js
@@ -20,7 +20,7 @@ const lostItemsSchema =new mongoose.Schema({
 	category: {
 		type:String,
 		required:true,
-		enum: ['Electronics', 'Clothing', 'Accessories', 'Books', 'Jewllery', 'Others'] 
+		enum: ['Electronics', 'Clothing', 'Accessories', 'Books', 'Jwellery', 'Others'] 
 	},
 	description: {
 		type: String,
